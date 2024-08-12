395a62aa26c13460b01fd6995ae93e9b0a8f2856
mohith1490 <mohith1754singh@gmail.com>
2024-08-12 09:56:42 +0530

    added auth backend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import myuserModel from './schema/schema.js'
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 dotenv.config()
 
 const app = express();
@@ -33,17 +34,28 @@ try {
 }
 
 
-app.post('/',(req,res)=>{
+app.post('/',async (req,res)=>{
  let {firstname, lastname, email, password}  = req.body
- myuserModel.create({firstname,lastname,email,password})
- res.status(200).send('recived')
+ let user = await myuserModel.create({firstname,lastname,email,password})
+ console.log(user._id);
+ let secretkey = process.env.JWT_SECRET_KEY 
+ 
+ const token = jwt.sign({userid: user._id},secretkey,{
+   expiresIn: '3h'
+ })
+
+//  const verifytoken = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NmI5NmZhMzM5N2FkMDg2ZjM5N2NiM2IiLCJpYXQiOjE3MjM0Mjg3NzIsImV4cCI6MTcyMzQzOTU3Mn0.IsJRhk4db5uFK2b6Tvbr8DpU7Pbd-jA7LyjvbV5wBQI',secretkey)
+//   console.log(verifytoken);
+ 
+ res.status(200).json({token}) 
 })
 
 
 io.on("connection", (socket) => {
    console.log('socket id',socket.id);
-   socket.on('usermoves', ([from,to,piece]) => {
+   socket.on('usermoves', ([from,to,piece,iswhite]) => {
+      let turnblack = !iswhite
       console.log('sending the coordinates ', [from,to,piece]);      
-      socket.emit('move2',[from,to,piece])
+      socket.emit('move2',[from,to,piece,turnblack])
    })  
 })
