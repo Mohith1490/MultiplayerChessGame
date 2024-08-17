16871b116f1029a61765c218b2d97e7046e0cb8f
mohith1490 <mohith1754singh@gmail.com>
2024-08-17 10:52:01 +0530

    added token to local storage

--- backend/autnverification/userverfiication.js
+++ backend/autnverification/userverfiication.js
@@ -1,6 +1,21 @@
 import jwt from "jsonwebtoken";
-
+import dotenv from'dotenv'
+dotenv.config()
 export default function userverification(req,res,next){
-console.log(req.body);
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    }
+
+    try {
+        // Verify the token (assuming JWT is used)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY );
+        req.user = decoded; // Attach user information to the request object
+        
+        next(); // Proceed to the next middleware or route handler
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }
 
 }
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import { Server } from "socket.io";
 import myuserModel from './schema/schema.js'
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import userverification from './autnverification/userverfiication.js'
 dotenv.config()
 
 const app = express();
@@ -37,22 +38,15 @@ try {
 app.post('/',async (req,res)=>{
  let {firstname, lastname, email, password}  = req.body
  let user = await myuserModel.create({firstname,lastname,email,password})
- console.log(user._id);
- let secretkey = process.env.JWT_SECRET_KEY 
- 
- const token = jwt.sign({userid: user._id},secretkey,{
-   expiresIn: '3h'
- })
-
-//  const verifytoken = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NmI5NmZhMzM5N2FkMDg2ZjM5N2NiM2IiLCJpYXQiOjE3MjM0Mjg3NzIsImV4cCI6MTcyMzQzOTU3Mn0.IsJRhk4db5uFK2b6Tvbr8DpU7Pbd-jA7LyjvbV5wBQI',secretkey)
-//   console.log(verifytoken);
- 
- res.status(200).json({token}) 
+ let secretkey = process.env.JWT_SECRET_KEY  
+ const token = jwt.sign({userid: user._id},secretkey) 
+ res.json({token}) 
 })
+
+app.use('/game',userverification)
 
 
 io.on("connection", (socket) => {
-   console.log('socket id',socket.id);
    socket.on('usermoves', ([from,to,piece,iswhite]) => {
       let turnblack = !iswhite
       console.log('sending the coordinates ', [from,to,piece]);      
